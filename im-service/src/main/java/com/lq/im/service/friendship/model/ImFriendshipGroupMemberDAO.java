c0e05d6244dbe5ea817bdb9bca434c055368b0c1
liqi <973129554@qq.com>
2023-05-31 15:46:51 +0800

    feat: Add friendship group member entity

---
+++ im-service/src/main/java/com/lq/im/service/friendship/model/ImFriendshipGroupMemberDAO.java
@@ -0,0 +1,31 @@
+package com.lq.im.service.friendship.model;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 好友分组成员
+ * @ClassName: ImFriendshipGroupMemberDAO
+ * @Author: LiQi
+ * @Date: 2023-05-31 15:45
+ * @Version: V1.0
+ * @Description:
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("im_friendship_group")
+public class ImFriendshipGroupMemberDAO {
+
+    /**
+     * 好友分组id
+     */
+    private Long groupId;
+
+    /**
+     * 好友id
+     */
+    private String userId;
+}
