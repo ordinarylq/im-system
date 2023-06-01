2a201845ca82e11f0b5a1078d46ffc1280becc6b
liqi <973129554@qq.com>
2023-06-01 14:33:09 +0800

    feat: Add group member mapper

---
+++ im-service/src/main/java/com/lq/im/service/group/mapper/ImGroupMemberMapper.java
@@ -0,0 +1,13 @@
+package com.lq.im.service.group.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lq.im.service.group.model.ImGroupMemberDAO;
+
+/**
+ * @ClassName: ImGroupMemberMapper
+ * @Author: LiQi
+ * @Date: 2023-06-01 14:32
+ * @Version: V1.0
+ */
+public interface ImGroupMemberMapper extends BaseMapper<ImGroupMemberDAO> {
+}
